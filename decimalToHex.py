0816cb9557b64fdf6b85dace80824959b5a3f460
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-17 17:19:52 +0100

    Update decimalToHex.py

--- decimalToHex.py
+++ decimalToHex.py
@@ -1,4 +1,5 @@
 # take string of binary from decimal to binary scriptgit
+from itertools import count
 from math import ceil
 
 data = input('Please enter in the decimal you want to be converted to Hexadecimal: ')
@@ -29,7 +30,7 @@ temp_state = False
 integer_array = []
 decimal_state = []
 
-# gets remainders and puts them in the ineger_array
+# gets remainders and puts them in the integer_array
 while temp_state == False:
     if x == 0:
         temp_state = True
@@ -38,7 +39,28 @@ while temp_state == False:
         x = x // 16
         integer_array.append(remainder)
 
-for x in range(len(integer_array), -1, -1):
-    
+for x in range(len(integer_array)-1, -1, -1):
+    temp2_state = False
+    counter = 0
+    while temp2_state == False:
+        if integer_array[x] == hex_array[counter][1]:
+            temp2_state = True
+            integer_array[x] = hex_array[counter][0]
+        else:
+            temp2_state = False
+            counter = counter + 1
+
+integer_array = list(reversed(integer_array))
+
+# for Y - will only allow exact fractions of 16 e.g. 0.625 or 0.5
+temp_state = False
+z = int(ceil(y * 16))
+decimal_value = hex_array[z][0]
+
+output = str(''.join(integer_array)) + str('.') + str(decimal_value)
+print(output)
+
+
+
 
 
