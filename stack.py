8417bacde3a987e9511840e9fd8fc7023b840012
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-17 11:46:12 +0100

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,55 +1,21 @@
-# without an OOP approach
-print('------------ Stack ------------')
-stack_length = input('Please enter in the max length of the stack: ')
-print('------------------')
+class myStack:
+    def __init__(self, size):
+        # Initialises an array of size 'size' where each element is 'empty' and sets the head to -1
+        self.size = size
+        self.stack = ['Empty'] * size
+        self.head = -1
 
-myStack = ['Empty'] * int(stack_length)
-front = stack_length - 1
-size = 0
+    def showStack(self):
+        # outputs the contents of the stack
+        for i in self.stack:
+            print(i)
 
-def is_Full():
-    if size == stack_length - 1:
-        return True
-    else:
-        return False
-def is_Empty():
-    if size == 0:
-        return True
-    else:
-        return False
-
-def show_stack():
-    print('------------------')
-    print('Showing contents of the stack...')
-    counter = 0
-    while counter != stack_length - 1:
-        print(myStack[counter])
-        counter += 1
-    print('------------------')
-
-def push(data):
-    if is_Full():
-        print('Error - stack overflow')
-    else:
-        front -= 1
-        myStack[front] = data
-        size += 1
-        print('Data has been added to the top of the stack')
-
-def pop():
-    if is_Empty():
-        print('Error - stack underflow')
-    else:
-        data = myStack[front]
-        myStack[front] = 'Empty'
-        front += 1
-        size -= 1
+    def pop(self):
+        data = self.stack[self.head]
+        self.stack[self.head] = 'Empty'
+        self.head = self.head - 1
         return data
-
-def peek():
-    if is_Empty():
-        print('Error - stack underflow')
-    else:
-        data = myStack[front]
-        return data
-
+    
+    def peek(self):
+        return self.stack[self.head]
+    
