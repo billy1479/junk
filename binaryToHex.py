838c9ca3d2c7e0de6ec82f849de684fe75068845
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-18 09:50:37 +0100

    Update binaryToHex.py

--- binaryToHex.py
+++ binaryToHex.py
@@ -1,7 +1,14 @@
 # only lets up to 4 bits for the decimal part
 # as many bits for the integer part
+from email.mime import base
+
+
 print('Please ensure there are no more than 4 bits after the decimal point - any extra will be ignored.')
 binary_string = input('Please enter in the binary string: ')
+
+base_array = [8,4,2,1]
+hex_array = [['0',0], ['1',1], ['2',2],['3',3], ['4',4], ['5', 5], ['6',6], ['7', 7], ['8',8], ['9',9], ['A', 10],['B',11],['C',12],['D',13],['E',14],['F',15]]
+
 
 # This splits the binary string into integer and decimal part
 state = False
@@ -37,3 +44,11 @@ for value in x:
 
 general_array.append(temp_array)
 
+
+for i in range(0, len(general_array) - 1):
+    total = 0
+    for x in range(len(base_array)-1, -1, -1):
+        if general_array[i] == '1':
+            total = total + base_array[i]
+    hexValue = hex_array[total][1]
+            
