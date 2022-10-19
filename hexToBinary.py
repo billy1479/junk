fa163f03278b839a564997aca3aedfb13d9eb587
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-19 10:45:49 +0100

    Create hexToBinary.py

---
+++ hexToBinary.py
@@ -0,0 +1,26 @@
+# This converts a hex number (with decimal) to its binary equivalent
+base_array = [8,4,2,1]
+hex_array = [['0',0], ['1',1], ['2',2],['3',3], ['4',4], ['5', 5], ['6',6], ['7', 7], ['8',8], ['9',9], ['A', 10],['B',11],['C',12],['D',13],['E',14],['F',15]]
+
+myValue = input('Please enter in the hex value here: ')
+
+# split the string into the integer and decimal part
+
+
+
+# calculate sum of all integer values
+
+
+
+
+# calculate the sum of the decimal values
+
+
+
+
+# join together with a decimal point
+
+
+
+
+# output
