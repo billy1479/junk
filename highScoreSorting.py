5c955d24b24b8e8681df50a582dc2d280c2083f3
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-21 15:27:31 +0100

    Update highScoreSorting.py

--- highScoreSorting.py
+++ highScoreSorting.py
@@ -5,20 +5,19 @@ def highScoreInsertion(H, E):
     while myState == False:
         if E[0] > H[counter][0]:
             # This loops through the rest of the array and moves the values up by 1 to make room for the new element
-            if H[9] == 'Null':
+            if H[9] == 'NULL':
                 # This shifts all the values up by 1 and makes room for the new element
                 for x in range(8, counter - 1, -1):
                     H[x+1] = H[x]
                 H[counter] = E
             else:
                 deletedValue = H[9]
-                print('Sorry - ' + str(H[9][0]) + ' has had their highscore removed')
+                print('Sorry - ' + str(deletedValue) + ' has had their highscore removed')
                 H[9] = 'Null'
                 for x in range(8, counter - 1, -1):
                     H[x+1] = H[x]
                 H[counter] = E
             myState = True
-
         else:
             # This stores the next value in the array as value and increments the counter to point to the next value
             value = H[counter][0]
@@ -27,4 +26,5 @@ def highScoreInsertion(H, E):
 
 mainArray = [[940,'Mike'],[880,'Rob'],[830,'Jill'],[790,'Paul'],[750,'Anna'],[660,'Rose'],[650,'Jack'],'NULL','NULL','NULL']
 newAddition = [900, 'Billy']
+print(mainArray)
 print(highScoreInsertion(mainArray, newAddition))
