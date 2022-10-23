7d16f42642d2cba1784320b73bd2b782dae22725
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-10-23 17:55:41 +0100

    Update doubleLinkedList.py

--- doubleLinkedList.py
+++ doubleLinkedList.py
@@ -1,16 +1,10 @@
 # This is a double linked list where each node will have a next and previous pointer
 class Node:
-    def __init__(self, data):
-        self.next = None
-        self.previous = None
+    def __init__(self, data, x, y):
+        self.next = x
+        self.previous = y
         self.value = data
 
-    def getNext(self):
-        return self.next
-
-    def getData(self):
-        return self.value
-
-    def getPrevious(self):
-        return self.previous
-
+class doubleLinkedList:
+    def __init__(self):
+        self.header = Node
