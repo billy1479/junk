019ab13e77713f417d77e4681fcd5095280bbdb2
billy1479 <59696174+billy1479@users.noreply.github.com>
2022-11-10 16:22:04 +0000

    MCS week 6 practical

---
+++ MCS/question1.1.py
@@ -0,0 +1,14 @@
+import random
+import math
+
+# This is from linear algebra week 6
+matrix1 = []
+matrix2 = []
+
+# fills out matrix 1
+for x in range(0,2):
+    matrix1.append(random.randint(-5,5))
+    matrix2.append(random.randint(-5,5))
+
+print(matrix1)
+print(matrix2)
