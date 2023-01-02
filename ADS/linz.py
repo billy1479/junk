9503aa91ac8242d3578c452a23748c813a0abe64
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-02 01:32:48 +0000

    m

--- ADS/linz.py
+++ ADS/linz.py
@@ -10,16 +10,13 @@ def linz_ordering_eight():
         return linzArray
     else:
         if len(linzArray) > 1:
-            print(numArray2)
-            x = random.randint(0,len(numArray2) - 1)
-            value = numArray2[x]
+            x = random.randint(0,len(numArray) - 1)
+            value = numArray[x]
             y = ((linzArray[len(linzArray) - 1]) - value) % len(linzArray)
             if y in testArray:
-                numArray2.remove(value)
                 return linz_ordering_eight()
             else:
                 numArray.remove(value)
-                numArray2 = numArray[:]
                 testArray.append(y)
                 linzArray.append(value)
                 return linz_ordering_eight()
@@ -27,11 +24,18 @@ def linz_ordering_eight():
             x = random.randint(0,len(numArray) - 1)
             value = numArray[x]
             numArray.remove(value)
-            # numArray2.remove(value)
             linzArray.append(value)
             return linz_ordering_eight()
 
 print(linz_ordering_eight())
 
-# -------------- end of attempt 1 ------------------------------
+# -------------- end of attempt 1 -----------------------
 
+def linz_ordering_eight2():
+    if len(linzArray) == 8:
+        return linzArray
+    else:
+        if len(linzArray) > 1:
+            pass
+        else:
+            
