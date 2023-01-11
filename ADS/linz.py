48ec57d429b28c7bd4063a9d40839d9281eed76b
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-11 17:13:14 +0000

    global variables

--- ADS/linz.py
+++ ADS/linz.py
@@ -1,108 +1,33 @@
-# attempt 1
-# this works but need to sort out issue with recursion maxing out because the random numbers keep repeating
-
 import random
-numArray = [0,1,2,3,4,5,6,7]
-linzArray = []
-testArray = []
-
-
-def linz_ordering_eight():
-    global numArray
-    global testArray
-    if len(linzArray) == 8:
-        return linzArray
-    else:
-        if len(linzArray) > 1:
-            if len(numArray) == 0:
-                print('Number array has been restored')
-                value = linzArray.pop()
-                print('Popped value is ' + str(value))
-                numArray = [0,1,2,3,4,5,6,7]
-                for x in linzArray:
-                    if x in numArray:
-                        numArray.remove(x)
-                testArray = []
-                return linz_ordering_eight()
-            else:
-                x = random.randint(0,len(numArray) - 1)
-                value = numArray[x]
-                y = ((linzArray[len(linzArray) - 1]) - value) % 8
-                if y in testArray:
-                    numArray.remove(value)
-                    return linz_ordering_eight()
-                else:
-                    print(linzArray)
-                    numArray.remove(value)
-                    testArray.append(y)
-                    linzArray.append(value)
-                    return linz_ordering_eight()
-        else:
-            x = random.randint(0,len(numArray) - 1)
-            value = numArray[x]
-            numArray.remove(value)
-            linzArray.append(value)
-            return linz_ordering_eight()
-
-
-# def mainFunction():
-#     numArray = [0,1,2,3,4,5,6,7]
-#     linzArray = []
-#     testArray = []
-#     def linz_ordering_eight():
-#         print('this has run')
-#         print(linzArray)
-#         print(testArray)
-#         print(numArray)
-#         if len(linzArray) == 8:
-#             return linzArray
-#         else:
-#             if len(linzArray) > 1:
-#                 if len(numArray) == 0:
-#                     numArray = [0,1,2,3,4,5,6,7]
-#                 else:
-#                     x = random.randint(0,len(numArray) - 1)
-#                     value = numArray[x]
-#                     y = ((linzArray[len(linzArray) - 1]) - value) % len(linzArray)
-#                     if y in testArray or y == 0:
-#                         numArray.remove(value)
-#                         return linz_ordering_eight()
-#                     else:
-#                         numArray.remove(value)
-#                         testArray.append(y)
-#                         linzArray.append(value)
-#                         return linz_ordering_eight()
-#             else:
-#                 x = random.randint(0,len(numArray) - 1)
-#                 value = numArray[x]
-#                 numArray.remove(value)
-#                 linzArray.append(value)
-#                 return linz_ordering_eight()
-#     linz_ordering_eight()
-
-# print(mainFunction())
-
-
-
-
-
+# numArray = [0,1,2,3,4,5,6,7]
+# linzArray = []
+# testArray = []
 # def linz_ordering_eight():
+#     global numArray
+#     global testArray
 #     if len(linzArray) == 8:
 #         return linzArray
 #     else:
 #         if len(linzArray) > 1:
-#             if len(usedArray) == len(numArray):
-#                 linzArray.pop(len(linzArray) - 1)
-#                 usedArray = []
+#             if len(numArray) == 0:
+#                 print('Number array has been restored')
+#                 value = linzArray.pop()
+#                 print('Popped value is ' + str(value))
+#                 numArray = [0,1,2,3,4,5,6,7]
+#                 for x in linzArray:
+#                     if x in numArray:
+#                         numArray.remove(x)
+#                 testArray = []
+#                 return linz_ordering_eight()
 #             else:
 #                 x = random.randint(0,len(numArray) - 1)
 #                 value = numArray[x]
-#                 y = ((linzArray[len(linzArray) - 1]) - value) % len(linzArray)
-#                 if y in testArray or y == 0:
+#                 y = ((linzArray[len(linzArray) - 1]) - value) % 8
+#                 if y in testArray:
 #                     numArray.remove(value)
-#                     usedArray.append(value)
 #                     return linz_ordering_eight()
 #                 else:
+#                     print(linzArray)
 #                     numArray.remove(value)
 #                     testArray.append(y)
 #                     linzArray.append(value)
@@ -113,13 +38,53 @@ def linz_ordering_eight():
 #             numArray.remove(value)
 #             linzArray.append(value)
 #             return linz_ordering_eight()
-            
-            
 
+def linz_ordering_eight2():
+    global numArray
+    global testArray
+    global linzArray
+    testArray = []
+    linzArray = []
+    numArray = [0,1,2,3,4,5,6,7]
+    def myFunction():
+        global numArray
+        global testArray
+        global linzArray
+        if len(linzArray) == 8:
+            return linzArray
+        else:
+            if len(linzArray) > 1:
+                if len(numArray) == 0:
+                    print('Number array has been restored')
+                    value = linzArray.pop()
+                    print('Popped value is ' + str(value))
+                    numArray = [0,1,2,3,4,5,6,7]
+                    for x in linzArray:
+                        if x in numArray:
+                            numArray.remove(x)
+                    testArray = []
+                    return myFunction()
+                else:
+                    x = random.randint(0,len(numArray) - 1)
+                    value = numArray[x]
+                    y = ((linzArray[len(linzArray) - 1]) - value) % 8
+                    if y in testArray:
+                        numArray.remove(value)
+                        return myFunction()
+                    else:
+                        print(linzArray)
+                        numArray.remove(value)
+                        testArray.append(y)
+                        linzArray.append(value)
+                        return myFunction()
+            else:
+                x = random.randint(0,len(numArray) - 1)
+                value = numArray[x]
+                numArray.remove(value)
+                linzArray.append(value)
+                return myFunction()
+    return myFunction()
 
-
-
-
-print(linz_ordering_eight())
+print(linz_ordering_eight2())
 
 
