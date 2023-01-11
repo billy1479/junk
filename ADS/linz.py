b8a5081ec1b13a0dcb6227c0f73f41bdd11f6b8b
billy1479 <59696174+billy1479@users.noreply.github.com>
2023-01-11 17:29:06 +0000

    linz again

--- ADS/linz.py
+++ ADS/linz.py
@@ -85,6 +85,64 @@ def linz_ordering_eight2():
                 return myFunction()
     return myFunction()
 
-print(linz_ordering_eight2())
+# print(linz_ordering_eight2())
 
 
+def linz_ordering_k(k):
+    import random
+    global numArray
+    global testArray
+    global linzArray
+    numArray = []
+    testArray = []
+    linzArray = []
+    for x in range(0, k):
+        numArray.append(x)
+    def myFunction():
+        global numArray
+        global testArray
+        global linzArray
+        if len(linzArray) == k:
+            return linzArray
+        else:
+            if len(linzArray) > 1:
+                if len(numArray) == 0:
+                    value = linzArray.pop()
+                    for x in range(0, k):
+                        numArray.append(x)
+                    for x in linzArray:
+                        if x in numArray:
+                            numArray.remove(x)
+                    testArray = []
+                    return myFunction()
+                else:
+                    x = random.randint(0,len(numArray) - 1)
+                    value = numArray[x]
+                    y = ((linzArray[len(linzArray) - 1]) - value) % k
+                    if y in testArray:
+                        numArray.remove(value)
+                        return myFunction()
+                    else:
+                        numArray.remove(value)
+                        testArray.append(y)
+                        linzArray.append(value)
+                        return myFunction()
+            else:
+                x = random.randint(0,len(numArray) - 1)
+                value = numArray[x]
+                numArray.remove(value)
+                linzArray.append(value)
+                return myFunction()
+    return myFunction()
+
+# for x in range(6, 14):
+#     print(linz_ordering_k(x))
+print(linz_ordering_k(6))
+print(linz_ordering_k(7))
+print(linz_ordering_k(8))
+print(linz_ordering_k(9))
+print(linz_ordering_k(10))
+print(linz_ordering_k(11))
+print(linz_ordering_k(12))
+print(linz_ordering_k(13))
+print(linz_ordering_k(14))
